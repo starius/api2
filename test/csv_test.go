afa83c0d22d7e4258b83108826e9771fabbaedfd
Boris Nagaev <bnagaev@gmail.com>
2023-07-18 11:53:32 -0300

    csv: flush CSV writer and HTTP after every line
    This is needed to send them asap even if it takes time to get the first row.

--- test/csv_test.go
+++ test/csv_test.go
@@ -73,6 +73,71 @@ func TestCSV(t *testing.T) {
 	require.Equal(t, wantRows, results)
 }
 
+func TestSlowCSV(t *testing.T) {
+	type Request struct {
+	}
+
+	closeMeAfterGettingAlice := make(chan struct{})
+
+	getHandler := func(ctx context.Context, req *Request) (res *api2.CsvResponse, err error) {
+		rows := make(chan []string, 2)
+		go func() {
+			defer close(rows)
+			rows <- []string{"Alice", "31"}
+			<-closeMeAfterGettingAlice
+			rows <- []string{"Bob", "13"}
+		}()
+		return &api2.CsvResponse{
+			HttpCode:  http.StatusOK,
+			CsvHeader: []string{"Name", "Age"},
+			Rows:      rows,
+		}, nil
+	}
+
+	routes := []api2.Route{
+		{Method: http.MethodGet, Path: "/csv", Handler: getHandler, Transport: api2.CsvTransport},
+	}
+
+	mux := http.NewServeMux()
+	api2.BindRoutes(mux, routes)
+	server := httptest.NewServer(mux)
+	t.Cleanup(server.Close)
+
+	client := api2.NewClient(routes, server.URL)
+
+	req := &Request{}
+	res := &api2.CsvResponse{
+		Rows: make(chan []string),
+	}
+
+	var wg sync.WaitGroup
+
+	var results [][]string
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		for row := range res.Rows {
+			results = append(results, row)
+			if row[0] == "Alice" {
+				close(closeMeAfterGettingAlice)
+			}
+		}
+	}()
+
+	require.NoError(t, client.Call(context.Background(), res, req))
+
+	wg.Wait()
+
+	require.Equal(t, http.StatusOK, res.HttpCode)
+	require.Equal(t, []string{"Name", "Age"}, res.CsvHeader)
+
+	wantRows := [][]string{
+		{"Alice", "31"},
+		{"Bob", "13"},
+	}
+	require.Equal(t, wantRows, results)
+}
+
 func TestLargeCSV(t *testing.T) {
 	const lines = 1000000
 
